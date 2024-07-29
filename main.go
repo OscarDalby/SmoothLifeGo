ac8956cc0217fdf46df1bf10a9c9180611dd7ccb
Oscar Dalby <129061939+OscarDalby@users.noreply.github.com>
2024-07-29 16:41:00 +0100

    init

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"image"
+	"image/color"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+const screenWidth = 600
+const screenHeight = 600
+
+type Game struct {
+	img *image.RGBA
+}
+
+func NewGame(screenWidth int, screenHeight int) *Game {
+	return &Game{
+		img: image.NewRGBA(image.Rect(0, 0, screenWidth, screenHeight)),
+	}
+}
+
+func (g *Game) Update() error {
+	for x := 0; x < screenWidth; x++ {
+		for y := 0; y < screenHeight; y++ {
+			// this is just a static gradient at the mo
+			c := color.RGBA{
+				R: uint8((x + y) % 256),
+				G: uint8((x * y) % 256),
+				B: 128,
+				A: 255,
+			}
+			g.img.SetRGBA(x, y, c)
+		}
+	}
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.WritePixels(g.img.Pix)
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Continuous Domain Cellular Automaton")
+	if err := ebiten.RunGame(NewGame(screenWidth, screenHeight)); err != nil {
+		log.Fatal(err)
+	}
+}
